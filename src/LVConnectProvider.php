cfddb0eace69b345bd4b9b845b4f4cb884fe06b4
Mathieu TUDISCO <dev@mathieutu.ovh>
2019-01-09 18:22:41 +0100

    👽 Update to conform to last changes/standardisation.

--- src/LVConnectProvider.php
+++ src/LVConnectProvider.php
@@ -2,20 +2,19 @@
 
 namespace MathieuTu\LVConnectSocialite;
 
-use GuzzleHttp\ClientInterface;
-use Laravel\Socialite\Two\ProviderInterface;
+use Illuminate\Http\Request;
 use SocialiteProviders\Manager\OAuth2\AbstractProvider;
 use SocialiteProviders\Manager\OAuth2\User;
 
-class LVConnectProvider extends AbstractProvider implements ProviderInterface
+class LVConnectProvider extends AbstractProvider
 {
     const IDENTIFIER = 'LVCONNECT';
     protected $scopes = ['profile:get'];
     protected $scopeSeparator = ' ';
 
-    protected $url = 'https://lvconnect.link-value.fr/';
+    protected $url = 'https://lvconnect.link-value.fr';
 
-    public function __construct(\Illuminate\Http\Request $request, string $clientId, string $clientSecret, string $redirectUrl, array $guzzle = [])
+    public function __construct(Request $request, string $clientId, string $clientSecret, string $redirectUrl, array $guzzle = [])
     {
         parent::__construct($request, $clientId, $clientSecret, $redirectUrl, $guzzle);
 
@@ -32,51 +31,32 @@ class LVConnectProvider extends AbstractProvider implements ProviderInterface
         return $this->url('/oauth/token');
     }
 
-    protected function getUserByToken($token)
+    protected function getUserByToken($token): array
     {
-        $response = $this->getHttpClient()->get(
-            $this->url('/users/me'),
-            ['headers' => ['Authorization' => 'Bearer ' . $token]]
-        );
+        $response = $this->getHttpClient()->get($this->url('/users/me'), [
+            'headers' => ['Authorization' => 'Bearer ' . $token],
+        ]);
 
         return json_decode($response->getBody(), true);
     }
 
-    protected function mapUserToObject(array $user)
+    protected function mapUserToObject(array $user): User
     {
         return (new User())->setRaw($user)->map([
-            'id'     => $user['id'],
-            'name'   => $user['firstName'] . $user['lastName'],
-            'email'  => $user['email'],
+            'id' => $user['id'],
+            'name' => $user['firstName'] . $user['lastName'],
+            'email' => $user['email'],
             'avatar' => $user['profilePictureUrl'],
         ]);
     }
 
-    public function getAccessTokenResponse($code)
-    {
-        $postKey = (version_compare(ClientInterface::VERSION, '6') === 1) ? 'form_params' : 'body';
-
-        $response = $this->getHttpClient()->post($this->getTokenUrl(), [
-            'headers' => [
-                'Accept'        => 'application/json',
-                'Authorization' => 'Basic ' . base64_encode($this->clientId . ':' . $this->clientSecret),
-            ],
-            $postKey  => $this->getTokenFields($code),
-        ]);
-
-        return json_decode($response->getBody(), true);
-    }
-
-    protected function getTokenFields($code)
-    {
-        return [
-            'code'       => $code,
-            'grant_type' => 'authorization_code',
-        ];
-    }
-
-    protected function url($uri)
+    private function url(string $uri): string
     {
         return rtrim($this->url, '/') . '/' . ltrim($uri, '/');
+    }
+
+    public static function additionalConfigKeys()
+    {
+        return ['url'];
     }
 }
